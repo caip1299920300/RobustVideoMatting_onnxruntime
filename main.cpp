c62d6279efbc246c0c0a622582e088e487b2e083
wucaipeng <wucaipeng@kfb.cn>
2023-11-10 18:45:58 +0800

    修改

--- main.cpp
+++ main.cpp
@@ -13,42 +13,38 @@ int main(int argc, char* argv[])
 	RVM demo("model/model.onnx");
 	// 可调参数
 	
-	string img_path;// 声明一个字符串，用于接受输入的图片或视频等
+	string img_path;
 	cv::Mat img;
-	unsigned char* aResultImg;        // 声明一个图片矩阵
-	while (1)
+	unsigned char* aResultImg;    
+	// 输入测试图片并显示
+	std::cout << "input_ImgPath:" << endl;
+	cin >> img_path;
+	img = cv::imread(img_path);
+	//cv::cvtColor(img, img, cv::COLOR_BGR2RGB);
+	
+	auto start = std::chrono::steady_clock::now();
+	try // 使用try，防止输入的图片地址为错误的情况
 	{
-		// 输入测试图片并显示
-		std::cout << "input_ImgPath:" << endl;
-		cin >> img_path;
-		img = cv::imread(img_path);
-		//cv::cvtColor(img, img, cv::COLOR_BGR2RGB);
+		int height = img.rows;
+		int width = img.cols;
+		aResultImg = new unsigned char[height*width*3];
 		
-		auto start = std::chrono::steady_clock::now();
-		try // 使用try，防止输入的图片地址为错误的情况
-		{
-			int height = img.rows;
-			int width = img.cols;
-			aResultImg = new unsigned char[height*width*3];
-			
-			/* 抠图函数调用 */
-			demo.detect(img.data,aResultImg,width,height);
-			
-			cv::Mat pmat(height, width, CV_8UC3, aResultImg); 
-			cv::imwrite("result.jpg",pmat);
-			cout << "succeed" << endl;
-		}
-		catch (const std::exception&)
-		{
-			cout << "图片输入地址有误!" << endl;
-		}
-		auto end = std::chrono::steady_clock::now();
-		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-		std::cout << "函数调用耗时：" << duration.count() << "毫秒" << std::endl;
+		/* 抠图函数调用 */
+		demo.detect(img.data,aResultImg,width,height);
 		
-		delete[] aResultImg;
-
+		cv::Mat pmat(height, width, CV_8UC3, aResultImg); 
+		cv::imwrite("result.jpg",pmat);
+		cout << "succeed" << endl;
 	}
+	catch (const std::exception&)
+	{
+		cout << "图片输入地址有误!" << endl;
+	}
+	auto end = std::chrono::steady_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+	std::cout << "函数调用耗时：" << duration.count() << "毫秒" << std::endl;
+	
+	delete[] aResultImg;
 	return 0;
 }
 
